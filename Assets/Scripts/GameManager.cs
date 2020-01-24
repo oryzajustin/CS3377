02a75cb1b2f4c043790acaea7a0770198178e661
Justin <jkoh8@uwo.ca>
2020-01-23 23:07:23 -0500

    tweaking, polish, added game manager to hide mouse and add restart and quit functionality

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private GameManager _instance;
+
+    public GameManager instance 
+    { 
+        get
+        {
+            return _instance;
+        } 
+    }
+
+    private void Awake()
+    {
+        if (this._instance != null && this._instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            this._instance = this;
+        }
+    }
+
+    private void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
